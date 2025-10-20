4d35b05a0bf6c0e10f975a46d8836d84c13cb898
ovezthaking <oliwerx12@gmail.com>
2025-10-20 03:12:20 +0200

    update App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,117 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import "./styles.css";
+
+import { use, useEffect, useState } from 'react';
+import UserForm from './components/UserForm';
+
 
 function App() {
+  const questions = [
+    {
+      question: "What's your favorite color?",
+      options: ["Red 🔴", "Blue 🔵", "Green 🟢", "Yellow 🟡"],
+    },
+  ];
+
+  const keywords = {
+    Fire: 'fire',
+    Water: 'water',
+    Earth: 'earth',
+    Air: 'air',
+  };
+
+  const elements = {
+    "Red 🔴": "Fire",
+    "Blue 🔵": "Water",
+    "Green 🟢": "Earth",
+    "Yellow 🟡": "Air",
+  };
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [element, setElement] = useState('');
+  const [artwork, setArtwork] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  function handleAnswer(answer){
+    setAnswers([...answers, answer]);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  
+  function handleUserFormSubmit(name){
+    setUserName(name);
+  };
+
+
+  function determineElement(answers){
+    const counts = {};
+    answers.forEach(function(answer){
+      const element = elements[answer];
+      counts[element] = (counts[element] || 0) +1;
+    });
+    return Object.keys(counts).reduce(function(a,b){
+      return counts[a] > counts[b] ? a : b
+    });
+  };
+
+  async function fetchArtwork(keyword){
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${keyword}`
+      );
+      
+      if(!response.ok) {
+        throw new Error('Failed to fetch results!');
+      }
+
+      const data = await response.json();
+      const objectIDs = data.objectIDs;
+
+      if (!objectIDs || objectIDs.length === 0) {
+        throw new Error('No artworks found for this keyword');
+      }
+
+      const objectResponse = await fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIDs[0]}`
+      );
+
+      if (!objectResponse.ok){
+        throw new Error('Failed to fetch artwork details!');
+      }
+
+      const objectData = await objectResponse.json();
+
+      setArtwork(objectData);
+    } catch(err){
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+  useEffect(
+    function(){
+      if (currentQuestionIndex === questions.length) {
+        const selectedElement = determineElement(answers);
+        setElement(selectedElement);
+        fetchArtwork(keywords[selectedElement])
+      }
+    },
+    [currentQuestionIndex]
+  );
+
+
+
+
   return (
     <div className="App">
       < Header />
