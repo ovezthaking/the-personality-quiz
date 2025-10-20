437ba19b80b29c2143de66c605d2951d6c256878
ovezthaking <oliwerx12@gmail.com>
2025-10-20 03:36:30 +0200

    Working site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import "./styles.css";
-
-import { use, useEffect, useState } from 'react';
+import Results from './components/Results';
+import { useEffect, useState } from 'react';
 import UserForm from './components/UserForm';
+import { Routes, Route } from 'react-router-dom';
+import { UserProvider } from './components/UserContext';
+import Question from './components/Question';
 
 
 function App() {
@@ -113,9 +115,30 @@ function App() {
 
 
   return (
-    <div className="App">
-      < Header />
-    </div>
+    <UserProvider>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<UserForm/>} />
+          <Route
+            path='/quiz'
+            element={
+              currentQuestionIndex < questions.length ? (
+                <Question
+                  question={questions[currentQuestionIndex].question}
+                  options={questions[currentQuestionIndex].options}
+                  onAnswer={handleAnswer}
+                />
+              ) : (
+                <Results element={element} artwork={artwork} />
+              )
+            }
+          />
+        </Routes>
+        {loading && <p>Loading artwork...</p>}
+        {error && <p>Error: {error}</p>}
+      </div>
+    </UserProvider>
   );
 }
 
--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -2,9 +2,12 @@ import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
-
 export function UserProvider({ children }) {
-    const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
-    return <UserContext.Provider vlaue={{ name, setName}}> {children} </UserContext.Provider>
+  return (
+    <UserContext.Provider value={{ name, setName }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
